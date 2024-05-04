85aa6697ef803846193ecdad5b2949a726d496da
Businge931 <jimmybusinge9@gmail.com>
2024-05-04 21:58:08 +0300

    feat: add animated counter

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import CountUp from "react-countup";
+
+
+const AnimatedCounter = () => {
+  return (
+    <div>
+      <CountUp end={100} />
+    </div>
+  );
+}
+
+export default AnimatedCounter
