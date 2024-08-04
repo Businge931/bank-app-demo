954b7b6d1b3088188a068d2a98323602d6b13ddf
Businge931 <jimmybusinge9@gmail.com>
2024-08-04 13:50:10 +0300

    prettier formatting

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,7 +1,5 @@
-
-
 import React from "react";
-// import { formatAmount } from "@/lib/utils";
+import { formatAmount } from "@/lib/utils";
 import AnimatedCounter from "./AnimatedCounter";
 import DoughnutChart from "./DoughnutChart";
 
@@ -12,7 +10,9 @@ const TotalBalanceBox = ({
 }: TotalBalanceBoxProps) => {
   return (
     <section className="total-balance">
-      <div className="total-balance-chart"><DoughnutChart accounts={accounts}/></div>
+      <div className="total-balance-chart">
+        <DoughnutChart accounts={accounts} />
+      </div>
       <div className="flex flex-col gap-6">
         <h2 className="header-2">Bank Accounts: {totalBanks}</h2>
         <div className="flex flex-col gap-2">
@@ -21,7 +21,7 @@ const TotalBalanceBox = ({
             <AnimatedCounter amount={totalCurrentBalance} />
           </div>
         </div>
-      </div> 
+      </div>
     </section>
   );
 };
